65de97cb584240e4caa39c724486b3b0cc792476
nic <nwanavit@hatari.cc>
2020-06-09 16:00:31 +0700

    change path for awscli in test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 docker run -v $(pwd)/test:/home/ thanakijwanavit/mkdocsaws sh -c 'cd test&&ls&&mkdocs build'
 echo test aws
-docker run -v $(pwd)/test:/home/ thanakijwanavit/mkdocsaws sh -c 'aws'
+docker run -v $(pwd)/test:/home/ thanakijwanavit/mkdocsaws sh -c '/usr/local/bin/aws --version'
