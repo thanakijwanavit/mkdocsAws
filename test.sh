c8c35c0ee92ec7fd9170aebd6a8016ec326d6deb
nic <nwanavit@hatari.cc>
2020-06-09 13:19:33 +0700

    chage test path

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd)/test:/home/ thanakijwanavit/mkdocsaws sh -c 'cd home&&ls&&mkdocs build'
+docker run -v $(pwd)/test:/home/ thanakijwanavit/mkdocsaws sh -c 'cd home/test&&ls&&mkdocs build'
